cc32168b8f061f05a231efe3df5dd6669bcb70d6
Vicente José Santiago Costa Oliveira <VJSCOliveira@ssp.riges>
2020-11-05 13:52:47 -0300

    implementação do sistema de cadastro e login com firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,10 +5,27 @@ import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    void _OnSuccess() {
+      Navigator.of(context).pop();
+    }
+
+    void _OnFail() {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+            content: Text("Falha ao entrar"),
+            backgroundColor: Colors.redAccent,
+            duration: Duration(seconds: 2)),
+      );
+    }
+
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Entrar"),
         centerTitle: true,
@@ -36,6 +53,7 @@ class LoginScreen extends StatelessWidget {
               padding: EdgeInsets.all(16.0),
               children: <Widget>[
                 TextFormField(
+                  controller: _emailController,
                   validator: (text) {
                     if (text.isEmpty || !text.contains("@"))
                       return "E-mail inválido";
@@ -45,6 +63,7 @@ class LoginScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 16.0),
                 TextFormField(
+                  controller: _passController,
                   validator: (text) {
                     if (text.isEmpty || text.length < 6)
                       return "Senha inválida";
@@ -56,7 +75,24 @@ class LoginScreen extends StatelessWidget {
                   alignment: Alignment.centerRight,
                   child: FlatButton(
                     padding: EdgeInsets.zero,
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_emailController.text.isEmpty)
+                        _scaffoldKey.currentState.showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  "Insira o e-mail para recuperar a senha"),
+                              backgroundColor: Colors.redAccent,
+                              duration: Duration(seconds: 2)),
+                        );
+                      else
+                        model.recoverPass(_emailController.text);
+                      _scaffoldKey.currentState.showSnackBar(
+                        SnackBar(
+                            content: Text("Confira seu e-mail"),
+                            backgroundColor: Theme.of(context).primaryColor,
+                            duration: Duration(seconds: 2)),
+                      );
+                    },
                     child: Text(
                       "Esqueci minha senha",
                       textAlign: TextAlign.right,
@@ -70,8 +106,11 @@ class LoginScreen extends StatelessWidget {
                     onPressed: () {
                       if (_formKey.currentState.validate()) {}
 
-                      model.signIn();
-
+                      model.signIn(
+                          email: _emailController.text.trim(),
+                          pass: _passController.text.trim(),
+                          onSuccess: _OnSuccess,
+                          onFail: _OnFail);
                     },
                     child: Text(
                       "Entrar",
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,67 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class SignUpScreen extends StatelessWidget {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _passController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    void _OnSuccess() {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+            content: Text("Usuário criado com sucesso!"),
+            backgroundColor: Theme.of(context).primaryColor,
+            duration: Duration(seconds: 2)),
+      );
+      Future.delayed(Duration(seconds: 2)).then((value) {
+        Navigator.of(context).pop();
+      });
+    }
+
+    void _OnFail() {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+            content: Text("Falha ao criar usuário"),
+            backgroundColor: Colors.redAccent,
+            duration: Duration(seconds: 2)),
+      );
+    }
+
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Criar Conta"),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            TextFormField(
-              validator: (text) {
-                if (text.isEmpty) return "Nome inválido";
-              },
-              decoration: InputDecoration(hintText: "Nome Completo"),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@"))
-                  return "E-mail inválido";
-              },
-              decoration: InputDecoration(hintText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              validator: (text) {
-                if (text.isEmpty) return "Endereço inválido";
-              },
-              decoration: InputDecoration(hintText: "Endereço"),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) return "Senha inválida";
-              },
-              obscureText: true,
-              decoration: InputDecoration(hintText: "Senha"),
-            ),
-            SizedBox(height: 16.0),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-                child: Text(
-                  "Criar Conta",
-                  style: TextStyle(fontSize: 18.0),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  validator: (text) {
+                    if (text.isEmpty) return "Nome inválido";
+                  },
+                  decoration: InputDecoration(hintText: "Nome Completo"),
                 ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-              ),
-            )
-          ],
-        ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _emailController,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail inválido";
+                  },
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _addressController,
+                  validator: (text) {
+                    if (text.isEmpty) return "Endereço inválido";
+                  },
+                  decoration: InputDecoration(hintText: "Endereço"),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _passController,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha inválida";
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: "Senha"),
+                ),
+                SizedBox(height: 16.0),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Map<String, dynamic> userData = {
+                          "name": _nameController.text.trim(),
+                          "email": _emailController.text.trim(),
+                          "address": _addressController.text.trim(),
+                        };
+
+                        model.signUp(
+                            userData: userData,
+                            pass: _passController.text.trim(),
+                            onSuccess: _OnSuccess,
+                            onFail: _OnFail);
+                      }
+                    },
+                    child: Text(
+                      "Criar Conta",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
